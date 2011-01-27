2428ef476fc0a58e2a367e76ac2b3eb1e7a764d3
Dave at Drinkmaster <ddean@drinkmaster.co.uk>
2011-01-27 11:38:42 +0000

    First set up of pages controller and index page.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  
+  # Return a title on a per-page basis.
+  def title
+    base_title = "Brandon Hire: Order Drinks"
+    @title ? base_title + " | " + @title : base_title
+  end
+  
+  # Return a body id on a per-page basis.
+  def body_id
+    @title ? @title.downcase : nil
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -49,7 +49,9 @@ BrandonApp::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
-
+  get 'pages/index'
+  root :to => 'pages#index'
+  
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
