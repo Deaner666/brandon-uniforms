57cee20ac41ffe7f9ff2c25ece0df46a5dd79b69
Dave at Drinkmaster <ddean@drinkmaster.co.uk>
2011-01-27 10:10:03 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BrandonApp::Application.config.secret_token = '1815d6fb23c4960bea24836b61b5b19513abd3238ba80411459b4215b6376b29b3697baa6c2bad9efad7b088000ef1e5dca867adaf58020eb1b91bb6d6e975db'
