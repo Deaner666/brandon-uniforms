fcfe3d7cebe25cafce79df175a70cf0a675434f4
Dave at Drinkmaster <ddean@drinkmaster.co.uk>
2011-02-08 15:51:36 +0000

    Added sales to list of email recipients cos I'd somehow managed to miss it off.

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -4,9 +4,8 @@ class OrderMailer < ActionMailer::Base
   
   def order_email(order)
     @order = order
-    #TODO Make this the correct email address at Brandon
     mail(:to => "bernard.french@brandonhire.co.uk, stationery@brandonhire.co.uk, tim.lethaby@brandonhire.co.uk",
-         :bcc => "ddean@drinkmaster.co.uk",
+         :bcc => "ddean@drinkmaster.co.uk, sales@drinkmaster.co.uk",
          :subject => "A Brandon Hire branch has placed a drinks order")
   end
   
