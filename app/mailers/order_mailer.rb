30d5b743dc878ed6e46325d29670143978638271
Dave at Drinkmaster <ddean@drinkmaster.co.uk>
2011-02-09 09:04:04 +0000

    Updated order mailer to only send confirmation emails to the stationery address.

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -4,7 +4,7 @@ class OrderMailer < ActionMailer::Base
   
   def order_email(order)
     @order = order
-    mail(:to => "bernard.french@brandonhire.co.uk, stationery@brandonhire.co.uk, tim.lethaby@brandonhire.co.uk",
+    mail(:to => "stationery@brandonhire.co.uk",
          :bcc => "ddean@drinkmaster.co.uk, sales@drinkmaster.co.uk",
          :subject => "A Brandon Hire branch has placed a drinks order")
   end
