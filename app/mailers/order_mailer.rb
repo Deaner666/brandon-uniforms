805850d09710441ff690e75e92980323737d1843
Dave at Drinkmaster <ddean@drinkmaster.co.uk>
2011-02-16 09:50:56 +0000

    Final try

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,12 +1,11 @@
 class OrderMailer < ActionMailer::Base
   
-  default :from => "sales@drinkmaster.co.uk"
-  
   def order_email(order)
     @order = order
-    mail(:to => "stationery@brandonhire.co.uk",
+    mail(:from => "sales@drinkmaster.co.uk",
+         :to => "stationery@brandonhire.co.uk",
          :bcc => "ddean@drinkmaster.co.uk, sales@drinkmaster.co.uk",
-         :subject => "A Brandon Hire branch has placed a drinks order")
+         :subject => "A drinks order has been placed by a Brandon Hire branch")
   end
   
 end
