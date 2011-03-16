e5975fd730a394b71eeebf2048a875875d8a8dd8
Dave at Drinkmaster <ddean@drinkmaster.co.uk>
2011-03-16 16:18:06 +0000

    Another crack at getting the Brandon stationery address to receive emails.

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -2,9 +2,9 @@ class OrderMailer < ActionMailer::Base
   
   def order_email(order)
     @order = order
-    mail(:from => "sales@drinkmaster.co.uk",
+    mail(:from => "ddean@drinkmaster.co.uk",
          :to => "stationery@brandonhire.co.uk",
-         :bcc => "ddean@drinkmaster.co.uk, sales@drinkmaster.co.uk",
+         :bcc => "lambcam@drinkmaster.co.uk, sales@drinkmaster.co.uk",
          :subject => "A drinks order has been placed by a Brandon Hire branch")
   end
   
