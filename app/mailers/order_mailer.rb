21b03c91369bb18de40430b4cdbba90ac7e56de2
Dave Dean <mail@dave-dean.co.uk>
2011-04-27 22:35:19 +0100

    Updated email addresses to their final production values.

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -2,8 +2,9 @@ class OrderMailer < ActionMailer::Base
   
   def order_email(order)
     @order = order
-    mail(:from => "info@monsoonmarketing.co.uk",
-         :to => "mail@dave-dean.co.uk",
+    mail(:from => "sales@monmark.co.uk",
+         :to => "stationery@brandonhire.co.uk",
+         :bcc => "mail@dave-dean.co.uk"
          :subject => "An order for uniforms has been placed by a Brandon Hire branch")
   end
   
