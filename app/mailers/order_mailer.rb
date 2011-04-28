f440bd6c07e8f36d5399d06d56c2cfd8885ee31e
Dave Dean <mail@dave-dean.co.uk>
2011-04-28 23:03:11 +0100

    Fixed syntax error in order mailer.

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -4,7 +4,7 @@ class OrderMailer < ActionMailer::Base
     @order = order
     mail(:from => "sales@monmark.co.uk",
          :to => "stationery@brandonhire.co.uk",
-         :bcc => "mail@dave-dean.co.uk"
+         :bcc => "mail@dave-dean.co.uk",
          :subject => "An order for uniforms has been placed by a Brandon Hire branch")
   end
   
