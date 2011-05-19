e108cc367ba679f84b395f936f2d314f75caea5b
Dave Dean <mail@dave-dean.co.uk>
2011-05-19 20:53:57 +0100

    Implement activeadmin gem and made initial config.

---
+++ app/admin/products.rb
@@ -0,0 +1,40 @@
+ActiveAdmin.register Product do
+  
+  form do |f|
+    f.inputs "Product Details" do
+      f.input :name
+      f.input :stock_code
+      f.input :price, :hint => "In pence"
+      f.input :sizes, :as => :select, :hint => "You can select multiple sizes"
+    end
+    f.buttons
+  end
+  
+  show do
+    # title product.name
+    # div do
+    #   p product.stock_code
+    #   p product.price
+    # end
+    
+    # ul do
+    #       for size in product.sizes do
+    #         li size.name
+    #       end
+    #     end
+    
+    # main_content
+    
+    attributes_table do
+      rows :name, :stock_code, :price
+      row("Sizes") do
+        ul do
+          for size in product.sizes do
+            li size.name
+          end
+        end
+      end
+    end
+  end
+  
+end
