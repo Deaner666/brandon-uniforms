558f65a236f3f55293081c746c36631a92852816
Dave Dean <mail@dave-dean.co.uk>
2011-05-20 20:05:36 +0100

    Included sizes in the @products instance variable on store index page.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,7 +6,7 @@ class ProductsController < ApplicationController
   # GET /products.xml
   def index
     @title = "Order Uniforms"
-    @products = Product.all
+    @products = Product.all :include => :sizes
     respond_to do |format|
       format.html # index.html.erb
       format.xml  { render :xml => @products }
