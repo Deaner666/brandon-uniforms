9a9f95eaa4dde13e20043bd0692687a1808a84c0
Dave Dean <mail@dave-dean.co.uk>
2011-05-23 18:26:03 +0100

    Updated order mailer to send to final production values.

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -3,7 +3,8 @@ class OrderMailer < ActionMailer::Base
   def order_email(order)
     @order = order
     mail(:from => "sales@monmark.co.uk",
-         :to => "mail@dave-dean.co.uk",
+         :to => "stationery@brandonhire.co.uk",
+         :cc => "sales@monmark.co.uk",
          :bcc => "mail@dave-dean.co.uk",
          :subject => "An order for uniforms has been placed by a Brandon Hire branch")
   end
