4b7a2ebb094c78f2cf3a49ecbefd4c276be48914
Dave Dean <mail@dave-dean.co.uk>
2011-06-01 18:37:01 +0100

    Fixed bug introduced by adding job_title to orders model.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
     if session[:order]
       @order = Order.find_by_id(session[:order], :include => :line_items)
     else
-      @order = Order.create!(:branch => "temp", :contact => "temp")
+      @order = Order.create!(:branch => "temp", :contact => "temp", :job_title => "temp")
       session[:order] = @order.id
     end
   end
